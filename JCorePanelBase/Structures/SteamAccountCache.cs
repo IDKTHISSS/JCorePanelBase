1a520191308aebb6c16079d2365da599ff316f9f
IDKTHISSS <polireek@gmail.com>
2023-11-04 22:19:11 +0200

    small update

--- JCorePanelBase/Structures/SteamAccountCache.cs
+++ JCorePanelBase/Structures/SteamAccountCache.cs
@@ -9,6 +9,6 @@ namespace JCorePanelBase
     public class SteamAccountCache
     {
         public string Nickname = "Name";
-        public string AvatarURL = "https://avatars.cloudflare.steamstatic.com/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg";
+        public byte[] AvatarBytes;
     }
 }
